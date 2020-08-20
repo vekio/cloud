0969c62904ff665b4680518c4ea687d2ab5fd719
vekio <alberto@casta.me>
2020-08-20 17:38:52 +0200

    Update freshrss db

--- databases/db-freshrss.sh
+++ databases/db-freshrss.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER freshrss;
     CREATE DATABASE freshrss;
+    CREATE USER freshrss with encrypted password 'freshrss';
     GRANT ALL PRIVILEGES ON DATABASE freshrss TO freshrss;
 EOSQL
